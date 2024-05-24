516dd8432d4b12bae92088e0ca68d1c662a09d4e
divine016 <koutidivine@gmail.com>
2024-05-24 08:25:21 +0100

    chnaged the file name

---
+++ Opportunity-Board/resources/views/mails/apply-opp-mail.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>sombody applid for your opportunity</p>
+
+    <button> <a href="{{ route('cpmpany') }}">click here to see it</a></button>
+
+    <p>thank youuu?</p>
+</body>
+</html>
